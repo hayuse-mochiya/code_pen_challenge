965a40be314159881c7972c0a68d75cef5f8157f
karita <hayue0228@icloud.com>
2024-06-05 15:10:10 +0900

    project init

---
+++ codepen/src/main.ts
@@ -0,0 +1,8 @@
+import './assets/scss/style.scss'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = ``
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
